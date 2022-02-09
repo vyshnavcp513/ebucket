f3ef2a2b9ca17e136eef6806c6e53c59b7fddfe1
vyshnavcp513 <vyshnavcp513@gmail.com>
2022-02-09 21:48:36 +0530

    login ui designed but bacground image doesnot fit correctly

--- lib/common/loginpage.dart
+++ lib/common/loginpage.dart
@@ -23,152 +23,208 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Form(
-          key: _loginkey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Image.asset(
-                    'images/logo.png',
-                    height: 100,
-                    width: 100,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: emailinputcontroller,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
+        child: Container(
+          decoration: BoxDecoration(
+            // color: Colors.grey.shade200.withOpacity(0.5),
+              image: DecorationImage(
 
-                      labelText: 'Email',
-                      hintText: 'abc@gmail.com',
-                      prefixIcon: Icon(Icons.email),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                    ),
-                    validator: (value) {
-                      if (value!.length <= 3) return 'Invalid Username';
-                    },
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: passwordinputcontroller,
-                    obscureText: _isHidden,
-                    // keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.lock),
-                      suffix: InkWell(
-                        onTap: _togglePasswordView,
-                        child: Icon(
-                          _isHidden ? Icons.visibility : Icons.visibility_off,
-                        ),
+                  fit: BoxFit.cover,
+                  image: AssetImage('images/backimage1.5.png')
+              )
+          ),
+
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Colors.white,
+                      Colors.transparent
+
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter
+                )
+            ),
+            child: Form(
+              key: _loginkey,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
                       ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                    ),
-                    validator: (value) {
-                      if (value!.length < 4)
-                        return 'Password should contain 4 characters!';
-                    },
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
+                      Image.asset(
+                        'images/logo.png',
+                        height: 100,
+                        width: 100,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: emailinputcontroller,
+                        keyboardType: TextInputType.emailAddress,
 
-                  Container(
-                    // decoration: BoxDecoration(
-                    //   borderRadius: BorderRadius.circular(80),
-                    // ),
-                    width: 150,
-                    height: 40,
-                    child: ElevatedButton.icon(
-                        onPressed: () {
-                          if (_loginkey.currentState!.validate()) {
-                            if (emailinputcontroller.text ==
-                                    "admin" &&
-                                passwordinputcontroller.text == "1234")
-                              Navigator.pushReplacement(
+                        decoration: InputDecoration(
+                          enabledBorder:OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+
+                            borderRadius: BorderRadius.circular(20),
+
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+
+                            borderRadius: BorderRadius.circular(10),
+
+                          ),
+
+                          fillColor: Colors.white70,
+                          filled: true,
+                          labelText: 'Email',
+                            hintText: 'abc@gmail.com',
+                          labelStyle: TextStyle(color: Colors.black87),
+                          prefixIcon: Icon(Icons.email,color: Colors.black,),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                        validator: (value) {
+                          if (value!.length <= 3) return 'Invalid Username';
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: passwordinputcontroller,
+                        obscureText: _isHidden,
+                        // keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+
+                            borderRadius: BorderRadius.circular(20),
+
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          fillColor: Colors.white70,
+                          filled: true,
+
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.black87),
+                          prefixIcon: Icon(Icons.lock,color: Colors.black),
+                          suffix: InkWell(
+                            onTap: _togglePasswordView,
+                            child: Icon(
+                              _isHidden ? Icons.visibility : Icons.visibility_off,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                        validator: (value) {
+                          if (value!.length < 4)
+                            return 'Password should contain 4 characters!';
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Container(
+                        // decoration: BoxDecoration(
+                        //   borderRadius: BorderRadius.circular(80),
+                        // ),
+                        width: 150,
+                        height: 40,
+                        child: ElevatedButton.icon(
+                            style:ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)) ,
+                            onPressed: () {
+                              if (_loginkey.currentState!.validate()) {
+                                if (emailinputcontroller.text ==
+                                        "admin" &&
+                                    passwordinputcontroller.text == "1234")
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => AdminHome()));
+                                else{
+                                  FirebaseAuth.instance.signInWithEmailAndPassword(email: emailinputcontroller.text, password: passwordinputcontroller.text).
+                                  then((value) => FirebaseFirestore.instance.collection('user').doc(value.user!.uid).get().
+                                  then((value) {
+                                    if(value.data()!['status']==1&&value.data()!['usertype']=='User')
+                                      {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => UserLandingPage(
+                                                  uid:value.data()!['uid'] ,
+                                                  address: value.data()!['address'],
+                                                  name: value.data()!['name'],
+                                                  location: value.data()!['location'],
+                                                  phone: value.data()!['phone'],
+                                                  email: value.data()!['email'],
+                                                  category: value.data()!['usertype'],
+
+
+                                                )));
+                                      }else if(value.data()!['status']==1&&value.data()!['usertype']=='Collecting Agent')
+                                        {
+                                          Navigator.pushReplacement(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => AgentLandingPage(
+                                                    agentid:value.data()!['uid'] ,
+                                                    agentname: value.data()!['name'],
+                                                    address: value.data()!['address'],
+                                                    name: value.data()!['name'],
+                                                    location: value.data()!['location'],
+                                                    phone: value.data()!['phone'],
+                                                    email: value.data()!['email'],
+                                                    category: value.data()!['usertype'],
+                                                  )));
+                                        }
+                                    else
+                                      showsnackbar('Cannot login');
+
+                                  })).catchError((e)=>showsnackbar('Login failed'));
+                                }
+
+
+                              }
+                            },
+                            icon: Icon(Icons.login,),
+                            label: Text("Login")),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextButton(
+                            onPressed: () {
+                              Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => AdminHome()));
-                            else{
-                              FirebaseAuth.instance.signInWithEmailAndPassword(email: emailinputcontroller.text, password: passwordinputcontroller.text).
-                              then((value) => FirebaseFirestore.instance.collection('user').doc(value.user!.uid).get().
-                              then((value) {
-                                if(value.data()!['status']==1&&value.data()!['usertype']=='User')
-                                  {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => UserLandingPage(
-                                              uid:value.data()!['uid'] ,
-                                              address: value.data()!['address'],
-                                              name: value.data()!['name'],
-                                              location: value.data()!['location'],
-                                              phone: value.data()!['phone'],
-                                              email: value.data()!['email'],
-                                              category: value.data()!['usertype'],
-
-
-                                            )));
-                                  }else if(value.data()!['status']==1&&value.data()!['usertype']=='Collecting Agent')
-                                    {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => AgentLandingPage(
-                                                agentid:value.data()!['uid'] ,
-                                                agentname: value.data()!['name'],
-                                                address: value.data()!['address'],
-                                                name: value.data()!['name'],
-                                                location: value.data()!['location'],
-                                                phone: value.data()!['phone'],
-                                                email: value.data()!['email'],
-                                                category: value.data()!['usertype'],
-                                              )));
-                                    }
-                                else
-                                  showsnackbar('Cannot login');
-
-                              })).catchError((e)=>showsnackbar('Login failed'));
-                            }
-
-
-                          }
-                        },
-                        icon: Icon(Icons.login),
-                        label: Text("Login")),
+                                      builder: (context) => RegisterPage()));
+                            },
+                            child: Text(
+                              "New User? Register",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegisterPage()));
-                        },
-                        child: Text(
-                          "New User? Register",
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.lightBlueAccent,
-                              fontWeight: FontWeight.bold),
-                        )),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
